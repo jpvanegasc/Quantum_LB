b2820bbf310e48471a9867ec78dc79e85e3044b1
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2019-09-15 11:00:29 -0500

    Creates Quantum CA

---
+++ CA_Quantum.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include"Random64.h"
+using namespace std;
+
+const int Lx = 10;
+const int Ly = 10;
+
+class Automata{
+	private:
+		double juan[Lx][Ly];
+	public:
+		void inicie(void);
+		void esta_funcion_avanza_un_paso(void);
+		void show(void){
+			for (int i=0; i<Lx; i++)
+				for (int j=0; j<Ly; j++)
+					cout << juan[i][j];
+			
+		}
+};
+
+
+
+int main(){
+	return 0;
+}
