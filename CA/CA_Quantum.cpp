3806c612bf36853a20fbd5888970186acfa1ec0c
Juan Bernardo Benavides <jbbenavidesr@unal.edu.co>
2019-11-02 12:24:37 -0500

    Add normalize function

--- CA/CA_Quantum.cpp
+++ CA/CA_Quantum.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<fstream>
 #include<cmath>
+#include <complex> 
+
 #include"Random64.h"
 
 const int Lx = 30;
@@ -9,26 +11,46 @@ class Automata{
 	private:
 		double juan[Lx];
 	public:
-		Automata(void){
-			for(int i=0; i<Lx; i++){
-				juan[i] = 0;
-				if(i==15 || i==16)
-					juan[i] = 1.0/M_SQRT2;
-			}
-		}
+		Automata(void);
+		void normalize(void);
 		void collision(void);
 		void advection(void);
-		void show(void){
-			for (int i=0; i<Lx; i++)
-					std::cout << juan[i] << " | ";
-			std::cout << std::endl;
-		}
+		void show(void);
 };
 
 int main(void){
 	Automata QuantumStuffBaby;
 
+	QuantumStuffBaby.normalize();
+
 	QuantumStuffBaby.show();
 
 	return 0;
 }
+
+Automata::Automata(void){
+	for(int i=0; i<Lx; i++){
+		juan[i] = 0;
+		if(i==15 || i==16)
+			juan[i] = 1.0;
+	}
+}
+
+void Automata::normalize(void){
+	double sum = 0, rsum;
+
+	for (int i=0; i<Lx; i++){
+		sum += juan[i]*juan[i];
+	}
+
+	rsum= sqrt(sum);
+	for (int i=0; i<Lx; i++){
+		juan[i]= juan[i]/rsum;
+	}
+
+}
+void Automata::show(void){
+	for (int i=0; i<Lx; i++)
+		std::cout << juan[i] << " | ";
+		std::cout << std::endl;
+	}
