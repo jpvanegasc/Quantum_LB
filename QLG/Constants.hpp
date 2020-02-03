6d588d43b120f261740765f47c5f4c7b3e81411b
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2020-02-03 16:08:19 -0500

    Feat: Quantum lattice gas working

---
+++ QLG/Constants.hpp
@@ -0,0 +1,15 @@
+#include<complex>
+#include<cmath>
+
+const int Lx = 2048;
+const int dim = 2;
+
+const double theta = 0.463647609; 
+const double sin_theta = std::sin(theta);
+const double cos_theta = std::cos(theta);
+
+const std::complex <double> j (0,1);
+const std::complex <double> uno (1,0);
+
+const double beta = -0.2;
+const double k = std::tan(theta)*beta;
--- QLG/main.cpp
+++ QLG/main.cpp
@@ -4,16 +4,19 @@
 #include"Quantum_LG.hpp"
 
 int main(void){
-    Automata QuantumStuffBaby(1);
-    std::ofstream result_file1("datos1.txt");//, result_file2("datos2.txt");
-    int t_max = 100;
+    Automata QuantumStuffBaby(Lx/2.0, 50);
+    std::ofstream result_file1("datos1.txt"), result_file2("datos2.txt"), result_file3("datos3.txt");
+    int t_max = 2000;
+    QuantumStuffBaby.print(result_file1);
     
     for(int t=0; t<t_max; t++){
-        QuantumStuffBaby.grafique(result_file1, t);
-        QuantumStuffBaby.collision(0);
-        QuantumStuffBaby.collision(1);
+        QuantumStuffBaby.collide();
+        QuantumStuffBaby.advect();
+        if(t==t_max/2) QuantumStuffBaby.print(result_file2);
     }
-    
-    result_file1.close();// result_file2.close();
+
+    QuantumStuffBaby.print(result_file3);
+
+    result_file1.close(); result_file2.close(); result_file3.close();
     return 0;
 }
