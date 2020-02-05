fb6c046ce4b352cbff78ca2083deb49a4f464e74
Juan B Benavides <jbbenavidesr@unal.edu.co>
2020-02-04 20:41:49 -0500

    Feat: Implement two component psi. Evolution fails

--- CA/CA_Quantum.hpp
+++ CA/CA_Quantum.hpp
@@ -5,8 +5,11 @@ class Automata{
     private:
         std::complex<double> phi[Lx];
         std::complex<double> phi_new[Lx];
+        std::complex<double> psi[Lx][2];
+        std::complex<double> psi_new[Lx][2];
     public:
         Automata(unsigned long long seed, double mu, double sigma);
+        void defPsi(void);
         void normalize(void);
         void collision(int i_start);
         void advect(void);
@@ -19,34 +22,34 @@ class Automata{
 
 Automata::Automata(unsigned long long seed, double mu, double sigma){
     CRandom ran64(seed);
-    for(int i=0; i<Lx; i++){
-        if(i==0) phi[i] = std::complex <double>(1, 0);
-        else phi[i] = std::complex <double>(0, 0);
-        phi_new[i] = std::complex <double>(0, 0);
+    double x;
+    for(int ix=0; ix<Lx; ix++){
+        x = gauss_init(ix, mu, sigma);
+        phi[ix] = std::complex<double> (x*std::cos(k*ix), -x*std::sin(k*ix));
+        phi_new[ix] = psi[ix][0] = psi[ix][1] = psi_new[ix][0] = psi_new[ix][1] = std::complex <double>(0, 0);
     }
     normalize();
 }
+
+void Automata::defPsi(void){
+    for(int ix=0; ix<Lx; ix++){
+        psi[ix][0]= phi[(ix-1+Lx)%Lx];
+        psi[ix][1]= phi[ix];
+    }
+}
+
 void Automata::normalize(void){
     double sum{0};
     double newSum{0};
-
+    defPsi();
     #pragma omp parallel for reduction(+: sum)
     for (int i=0; i<Lx; i++)
-        sum += std::norm(phi[i]);
+        sum += std::norm(psi[i][0]) + std::norm(psi[i][1]);
     sum = sqrt(sum);
 
     #pragma omp parallel for
     for (int i=0; i<Lx; i++)
         phi[i] /= sum;
-
-    #pragma omp parallel for reduction(+: newSum)
-    for (int i=0; i<Lx; i++)
-        newSum += std::norm(phi_new[i]);
-    newSum = sqrt(newSum);
-
-    #pragma omp parallel for
-    for (int i=0; i<Lx; i++)
-        phi_new[i] /= newSum;
 }
 /**
  * Evolves one time step the state vector 
@@ -72,8 +75,9 @@ void Automata::grafique(int t, std::ofstream &file){
         file << i << "\t" << t << "\t" << std::norm(phi[i]) << "\n";
 }
 void Automata::wave(std::ofstream &file){
+    defPsi();
     for (int ix=0; ix<Lx; ix++){
-        file << ix << '\t' << std::norm(phi[ix]) << '\n';
+        file << ix << '\t' << std::norm(psi[ix][0]) + std::norm(psi[ix][1]) << '\n';
     }  
 }
 double Automata::gauss_init(int x, double mu, double sigma){
--- CA/main.cpp
+++ CA/main.cpp
@@ -3,19 +3,20 @@
 
 int main(void){
     Automata QuantumStuffBaby(1, Lx/2.0, 50);
-    std::ofstream result_file1("datos1.txt"), result_file2("datos2.txt"), result_file3("datos3.txt");
+    std::ofstream result_file1("datos1.txt"), result_file2("datos2.txt"), result_file3("datos3.txt"), result_file4("datos4.txt");
     
     QuantumStuffBaby.wave(result_file1);
     for(int t=0; t<t_max; t++){
-        QuantumStuffBaby.grafique(t, result_file3);
+        //QuantumStuffBaby.grafique(t, result_file4);
         QuantumStuffBaby.collision(1);
         QuantumStuffBaby.advect();
         QuantumStuffBaby.collision(0);
         QuantumStuffBaby.advect();
-        result_file3 << "\n";
+        //result_file4 << "\n";
+        if (t==t_max/2) QuantumStuffBaby.wave(result_file2);
     }
-    QuantumStuffBaby.wave(result_file2);
+    QuantumStuffBaby.wave(result_file3);
     
-    result_file1.close(); result_file2.close();
+    result_file1.close(); result_file2.close(); result_file3.close(); result_file4.close();
     return 0;
 }
